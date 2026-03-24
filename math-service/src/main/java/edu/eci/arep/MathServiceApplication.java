1cfd2ac095e6ae8b8d777563cb92ef3873b06e6f
JAPV-X2612 <jesus.pinzo-v@mail.escuelaing.edu.co>
2026-03-24 14:49:03 -0500

    build: Add new MathServiceApplication

---
+++ math-service/src/main/java/edu/eci/arep/MathServiceApplication.java
@@ -0,0 +1,24 @@
+package edu.eci.arep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the Math Service application.
+ *
+ * @author Jesús Pinzón
+ * @version 1.0
+ * @since 2026-03-24
+ */
+@SpringBootApplication
+public class MathServiceApplication {
+
+    /**
+     * Launches the Math Service Spring Boot application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MathServiceApplication.class, args);
+    }
+}
